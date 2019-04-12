07feb9c0d3d06b1784e7cc868a494da1f443dbc0
morimolymoly <morimolymoly@gmail.com>
2019-04-12 15:19:17 +0900

    enhanced ut

--- test/lex.cpp
+++ test/lex.cpp
@@ -1,19 +1,37 @@
 #include "../lex/lex.h"
 #include "../stubs/io.h"
 #include <stdio.h>
+#include <cassert>
+
+int UTLex()
+{
+    printf("[UT] Lex def\n");
+    MockInput("def fucker");
+    assert(get_tok() == tok_def);
+
+    printf("[UT] Lex extern\n");
+    MockInput("extern fucker");
+    assert(get_tok() == tok_extern);
+
+    printf("[UT] Lex def with some comments\n");
+    MockInput("#uooo!\ndef fucker");
+    assert(get_tok() == tok_def);
+
+    printf("[UT] Lex identifier\n");
+    MockInput("int");
+    assert(get_tok() == tok_identifier);
+
+    /* not implemented
+    printf("[UT] Lex number\n");
+    MockInput("400");
+    assert(get_tok() == tok_number);*/
+    return 0;
+}
 
 int main(void){
-
-    int actual;
-    char *test_def = "def fucker";
-    MockInput("def fucker");
     EnableMock();
-    actual = get_tok();
-    if(actual == -2){
-        printf("[UT] get_tok def is seems good\n");
-    }else{
-        printf("[UT] get_tok def seems bad\n");
+    if (UTLex() != 0)
         return -1;
-    }
+    printf("[UT] Lex seems good!\n");
     return 0;
 }
